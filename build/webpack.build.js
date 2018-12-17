fd6fc0936c9a494f857b45bc009703bbec51f99d
canfoo <canfoo@126.com>
2018-12-17 11:11:55 +0800

    publish 0.0.6

--- build/webpack.build.js
+++ build/webpack.build.js
@@ -23,7 +23,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '../dist'),
     filename: 'index.js',
-    library: 'tb-records',
+    library: 'DocReact',
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
